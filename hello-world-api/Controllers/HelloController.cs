f85e2d1364ae5d9ab4610fb2e6356d3228f79fde
David Scott Turner <david@letsqala.com>
2026-02-26 23:39:07 +0000

    feat: accept name parameter and write greeting to file
    The hello endpoint now takes a ?name= query parameter, returns
    "Hello world {name}", and writes the greeting to helloworld.txt
    on each request.

--- hello-world-api/Controllers/HelloController.cs
+++ hello-world-api/Controllers/HelloController.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.IO;
 using Microsoft.AspNetCore.Mvc;
 
 namespace hello_world_api.Controllers
@@ -9,12 +6,20 @@ namespace hello_world_api.Controllers
     [Route("api/[controller]")]
     public class HelloController : Controller
     {
-        // GET api/helllo
+        // GET api/hello?name=David
         [HttpGet]
-        public string Get()
+        public string Get([FromQuery] string name = "World")
         {
-            return "Hello world!!!!!!";
-        }
+            var message = $"Hello world {name}";
 
+            var dir = Directory.GetCurrentDirectory();
+            while (dir != null && Directory.GetFiles(dir, "*.sln").Length == 0)
+                dir = Directory.GetParent(dir)?.FullName;
+
+            var outputPath = Path.Combine(dir ?? Directory.GetCurrentDirectory(), "helloworld.txt");
+            System.IO.File.WriteAllText(outputPath, message);
+
+            return message;
+        }
     }
 }
